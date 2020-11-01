22b4f7c8d88788015c67e244b185f680b150b7f2
Byron Batteson <byronbatteson@gmail.com>
2020-11-01 20:00:53 +0200

    test(utils): add tests for determining editor
    Added tests around editor determination to increase code coverage.

--- src/utils.rs
+++ src/utils.rs
@@ -9,7 +9,7 @@ pub fn get_directory(note_directory: &str, entry_name: &str) -> String {
 
 pub fn get_datetime(time: &str, minute_bucket_size: u32) -> Result<DateTime<Local>, Error> {
     let now = Local::now();
-
+    
     match time {
         "now" => Ok(now.with_minute((now.minute() / minute_bucket_size) * minute_bucket_size).unwrap()),
         "tomorrow" => Ok(now.with_day(now.day() + 1).unwrap().with_minute((now.minute() / minute_bucket_size) * minute_bucket_size).unwrap()),
@@ -29,7 +29,7 @@ pub fn get_datetime(time: &str, minute_bucket_size: u32) -> Result<DateTime<Loca
             let is_meridiem_time = meridiem == "pm" || meridiem == "am";
             let is_pm = meridiem == "pm";
             let minute: u32 = if split.len() == 2 { split.get(1).unwrap().parse().unwrap() } else { 0 };
-
+            
             if is_meridiem_time && hour_digits > 12 {
                 bail!("invalid hour input for meridiem time");
             }
@@ -39,7 +39,7 @@ pub fn get_datetime(time: &str, minute_bucket_size: u32) -> Result<DateTime<Loca
             if minute > 59 {
                 bail!("invalid minute input")
             }
-
+            
             let hour: u32 = if is_meridiem_time {
                 if is_pm {
                     if hour_digits == 12 {
@@ -53,9 +53,9 @@ pub fn get_datetime(time: &str, minute_bucket_size: u32) -> Result<DateTime<Loca
             } else {
                 hour_digits
             };
-
+            
             let is_tomorrow = hour < now.hour() || (hour == now.hour() && minute < now.minute());
-
+            
             Ok(now
                 .with_day(now.day() + if is_tomorrow { 1 } else { 0 }).unwrap()
                 .with_hour(hour).unwrap()
@@ -75,8 +75,8 @@ pub fn get_editor() -> Result<String, Error> {
         },
         _ => {
             let vim_result = std::process::Command::new("which")
-                                                   .arg("vim")
-                                                   .output();
+            .arg("vim")
+            .output();
             match vim_result {
                 Ok(output) => {
                     if output.status.success() {
@@ -104,11 +104,40 @@ pub fn has_text(text: &str, lines: Lines<BufReader<fs::File>>) -> bool {
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
+    use std::{env::{remove_var, set_var}};
+    
+    #[test]
+    fn get_directory_appends_entry_name_to_directory() {
+        let directory = get_directory("/a/directory", "note");
+        assert_eq!(directory, "/a/directory/note");
+    }
+    
+    #[test]
+    fn get_editor_returns_editor_from_env_var() {
+        let original_editor = var("EDITOR");
 
-  #[test]
-  fn get_directory_appends_entry_name_to_directory() {
-    let directory = get_directory("/a/directory", "note");
-    assert_eq!(directory, "/a/directory/note");
-  }
+        set_var("EDITOR", "/usr/bin/fake_editor");
+        let editor = get_editor().unwrap();
+        assert_eq!(editor, "/usr/bin/fake_editor");
+
+        // restore environment variable state
+        match original_editor {
+            Ok(x) => set_var("EDITOR", x),
+            Err(_) => remove_var("EDITOR")
+        }
+    }
+    
+    #[test]
+    fn get_editor_returns_vim_or_errors_if_editor_env_var_empty() {
+        remove_var("EDITOR");
+        let editor = get_editor();
+        match editor {
+            Ok(x) => {
+                assert!(x.len() > 0);
+                assert!(x.contains("vim"));
+            },
+            Err(y) => assert_eq!(y.to_string(), "failed to determine editor")
+        }
+    }
 }
